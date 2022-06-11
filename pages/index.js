62c22ec097f6f03cfb7a3be86d67c72d696d315d
Cian Butler <ciandevelops@gmail.com>
2022-06-10 20:00:14 -0400

    main/hero done

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Main from '../components/Main'
 import Navbar from '../components/Navbar'
 
 export default function Home() {
@@ -12,6 +13,7 @@ export default function Home() {
       </Head>
 
       <Navbar/>
+      <Main/>
 
      
     </div>
