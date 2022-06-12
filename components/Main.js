3e8973ed9358e116718ef75554e224536a36484b
Cian Butler <ciandevelops@gmail.com>
2022-06-12 16:29:17 -0400

    basic skills done

--- components/Main.js
+++ components/Main.js
@@ -11,7 +11,7 @@ const Main = () => {
                 <p className='uppercase tracking-widest text-gray-600'>Let's build something together!</p>
                 <h1 className='py-4 text-gray-700'>Hi, I'm <span className='text-[#5651e5]'>Cian</span></h1>
                 <h1 className='py-2 text-gray-700'>A Front-End Web Developer</h1>
-                <p className='py-4 text-gray-600 max-w-[70%] m-auto'>I'm a self-taught front-end web developer based in Toronto. I specialize in building (and ocassionally designing) amazing digital experiences. Currently, I'm focused on building beautiful responsive front-end web applications while learning back-end technologies.</p>
+                <p className='py-4 text-gray-600 max-w-[70%] m-auto'>I'm a front-end web developer based in Toronto. I specialize in building (and ocassionally designing) amazing digital experiences. Currently, I'm focused on building beautiful responsive front-end web applications while learning back-end technologies.</p>
                 <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
                     <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
                         <FaLinkedinIn/>
