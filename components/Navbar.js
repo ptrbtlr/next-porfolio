63d708a15292ac158080ac537af712ba7dd5008e
Cian Butler <ciandevelops@gmail.com>
2022-06-12 21:29:04 -0400

    mobile nav closures fixed

--- components/Navbar.js
+++ components/Navbar.js
@@ -78,7 +78,10 @@ const Navbar = () => {
         >
           <div>
             <div className="flex w-full items-center justify-between">
-              <h1>Cian Butler</h1>
+              <Link href="/">
+                <h1>Cian Butler</h1>
+              </Link>
+
               <div
                 onClick={handleNav}
                 className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer"
@@ -95,19 +98,29 @@ const Navbar = () => {
           <div className="py-4 flex flex-col">
             <ul className="uppercase">
               <Link href="/">
-                <li className="py-4 text-sm">Home</li>
+                <li onClick={() => setNav(false)} className="py-4 text-sm">
+                  Home
+                </li>
               </Link>
-              <Link href="/">
-                <li className="py-4 text-sm">About</li>
+              <Link href="/#about">
+                <li onClick={() => setNav(false)} className="py-4 text-sm">
+                  About
+                </li>
               </Link>
-              <Link href="/">
-                <li className="py-4 text-sm">Skills</li>
+              <Link href="/#skills">
+                <li onClick={() => setNav(false)} className="py-4 text-sm">
+                  Skills
+                </li>
               </Link>
-              <Link href="/">
-                <li className="py-4 text-sm">Projects</li>
+              <Link href="/#projects">
+                <li onClick={() => setNav(false)} className="py-4 text-sm">
+                  Projects
+                </li>
               </Link>
-              <Link href="/">
-                <li className="py-4 text-sm">Contact</li>
+              <Link href="/#contact">
+                <li onClick={() => setNav(false)} className="py-4 text-sm">
+                  Contact
+                </li>
               </Link>
             </ul>
             <div className="pt-40">
---
+++ pages/crypto.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const crypto = () => {
+  return (
+    <div>
+        <h1>Crypto Page</h1>
+    </div>
+  )
+}
+
+export default crypto
