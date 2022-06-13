1a0a13b81720b9ca66a2d7c8c36cf137def61df8
Cian Butler <ciandevelops@gmail.com>
2022-06-12 21:03:32 -0400

    contact done

--- components/About.js
+++ components/About.js
@@ -28,7 +28,7 @@ const About = () => {
             quod! Odit porro iure aut quas illo?
           </p>
           <p className="py-2 text-gray-600 underline cursor-pointer">
-            Check out some of latest projects!
+            Check out some of my latest projects!
           </p>
         </div>
         <div className="w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300">
---
+++ components/Contact.js
@@ -0,0 +1,114 @@
+import Link from "next/link";
+import React from "react";
+import { AiOutlineMail } from "react-icons/ai";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
+import { HiOutlineChevronDoubleUp } from "react-icons/hi";
+
+const Contact = () => {
+  return (
+    <div className="w-full lg:h-screen">
+      <div className="max-w-[1240px] m-auto px-2 py-16 w-full">
+        <p className="text-xl tracking-widest uppercase text-[#5651e5]">
+          Contact
+        </p>
+        <h2 className="py-4">Get In Touch!</h2>
+        <div className="grid lg:grid-cols-5 gap-8">
+          {/* left */}
+          <div className="col-span-3 lg:col-span-2 w-full h-full shadow-xl shadow-gray-400 rounded-xl p-4">
+            <div className="lg:p-4 h-full">
+              <div>
+                <img
+                  src="https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80"
+                  alt="/"
+                  className="rounded-xl hover:scale-105 ease-in duration-300"
+                />
+              </div>
+              <div>
+                <h2 className="py-2">Cian Butler</h2>
+                <p>Front-End Developer</p>
+                <p className="py-4">
+                  I am available for full-time or freelance positions. Contact
+                  me!
+                </p>
+              </div>
+              <div>
+                <p className="pt-8 uppercase">Connect With Me!</p>
+                <div className="flex items-center justify-between py-4">
+                  <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+                    <FaLinkedinIn />
+                  </div>
+                  <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+                    <FaGithub />
+                  </div>
+                  <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+                    <AiOutlineMail />
+                  </div>
+                  <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+                    <BsFillPersonLinesFill />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* right */}
+          <div className="col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4">
+            <div className="p-4">
+              <form>
+                <div className="grid md:grid-cols-2 gap-4 w-full py-2 ">
+                  <div className="flex flex-col">
+                    <label className="text-sm uppercase py-2">Name</label>
+                    <input
+                      type="text"
+                      className="border-2 rounded-lg p-3 flex border-gray-300"
+                    />
+                  </div>
+                  <div className="flex flex-col">
+                    <label className="text-sm uppercase py-2">
+                      Phone Number
+                    </label>
+                    <input
+                      type="text"
+                      className="border-2 rounded-lg p-3 flex border-gray-300"
+                    />
+                  </div>
+                </div>
+                <div className="flex flex-col py-2">
+                  <label className="text-sm uppercase py-2">Email</label>
+                  <input
+                    type="email"
+                    className="border-2 rounded-lg p-3 flex border-gray-300"
+                  />
+                </div>
+                <div className="flex flex-col py-2">
+                  <label className="text-sm uppercase py-2">Subject</label>
+                  <input
+                    type="text"
+                    className="border-2 rounded-lg p-3 flex border-gray-300"
+                  />
+                </div>
+                <div className="flex flex-col py-2">
+                  <label className="text-sm uppercase py-2">Message</label>
+                  <textarea className="border-2 rounded-lg p-3 flex border-gray-300"></textarea>
+                </div>
+                <button className="w-full p-4 text-gray-100 mt-4">
+                  Send Message
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-center py-12">
+          <Link href="/">
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+              <HiOutlineChevronDoubleUp className="text-[#5651e5]" size={30} />
+            </div>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
