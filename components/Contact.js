ee0003ea96e78e333cf2363bc63ea3af5e090442
Cian Butler <67752829+ciandevelops@users.noreply.github.com>
2023-05-17 10:58:24 -0400

    resume is TBD

--- components/Contact.js
+++ components/Contact.js
@@ -87,7 +87,7 @@ const Contact = () => {
                       <AiOutlineMail />
                     </div>
                   </Link>
-                  <a download href="resume.pdf">
+                  <a download href="">
                     <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 hover:text-[#5651e5] ease-in duration-300">
                       <BsFillPersonLinesFill />
                     </div>
